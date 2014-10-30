66e87bb256959b95106451a9fbe988197c72d89c
coderaiser <mnemonic.enemy@gmail.com>
2014-10-30 05:49:01 -0400

    feature(publish) add

--- bin/publish.js
+++ bin/publish.js
@@ -3,54 +3,26 @@
 (function() {
     'use strict';
     
-    var spawnify    = require('spawnify'),
-        Util        = require('util-io'),
-        tryRequire  = require('tryrequire'),
-        
+    var publish     = require('../lib/publish'),
         args        = process.argv.slice(2),
+        arg         = args[0],
         
-        Cmd         = [ 'npm version {{ version }} -m "feature(package) v%s"',
-                        'git push origin master',
-                        'npm publish'
-                    ].join('&&'),
-        
-        MSG         = [ 'publish <version>',
-                        'package: {{ name }} v{{ version }}'
-                    ].join('\n'),
-                    
-        Info        = tryRequire(process.cwd() + '/package'),
-        
-        arg, cmd, version;
-        
-        arg = args[0];
+        version;
         
         if (arg === '-v' || arg === '--v') {
             version = require('../package').version;
             console.log('v' + version);
-        } else if (!Info)
-            console.error('package.json not found.');
-        else if (!args.length) {
-            console.log(Util.render(MSG, {
-                name    : Info.name,
-                version : Info.version
-            }));
-        } else {
+       } else {
             if (arg[0] === 'v')
                 version = arg.slice(1);
             else
                 version = arg;
             
-            cmd = Util.render(Cmd, {
-                version: version
-            });
-            
-            spawnify(cmd, function(error, json) {
-                var err = error && error.message || json.stderr;
-                
-                if (err)
-                    console.error(err);
+            publish(version, function(error, msg) {
+                if (error)
+                    console.error(error.message);
                 else
-                    console.log(json.stdout);
+                    console.log(msg);
             });
         }
     
