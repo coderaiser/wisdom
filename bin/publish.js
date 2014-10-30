df0d2ba310f08ebaf7216378f2059c7fb0c3b118
coderaiser <mnemonic.enemy@gmail.com>
2014-10-30 05:52:32 -0400

    fix(publish) no args

--- bin/publish.js
+++ bin/publish.js
@@ -6,24 +6,17 @@
     var publish     = require('../lib/publish'),
         args        = process.argv.slice(2),
         arg         = args[0],
-        
         version;
         
         if (arg === '-v' || arg === '--v') {
             version = require('../package').version;
             console.log('v' + version);
        } else {
-            if (arg[0] === 'v')
-                version = arg.slice(1);
-            else
-                version = arg;
-            
-            publish(version, function(error, msg) {
+            publish(arg, function(error, msg) {
                 if (error)
                     console.error(error.message);
                 else
                     console.log(msg);
             });
-        }
-    
+       }
 })();
