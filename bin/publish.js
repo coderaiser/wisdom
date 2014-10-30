a7c666d308a03efbd9cf4d2ca2ee39e8bae288eb
coderaiser <mnemonic.enemy@gmail.com>
2014-10-30 05:03:10 -0400

    feature(publish) change package.json, git tag -> npm version

--- bin/publish.js
+++ bin/publish.js
@@ -6,82 +6,52 @@
     var fs          = require('fs'),
         spawnify    = require('spawnify'),
         Util        = require('util-io'),
+        tryRequire  = require('tryrequire'),
         
         args        = process.argv.slice(2),
         
-        Cmd         = [ 'git add package.json',
-                        'git commit -m "feature(package) v{{ version }}"',
-                        'git push origin master',
-                        'git tag v{{ version }}',
-                        'git push origin v{{ version }}',
+        Cmd         = [ 'npm version {{ version }} -m "feature(package) v%s"',
                         'npm publish'
                     ].join('&&'),
         
         MSG         = [ 'publish <version>',
                         'package: {{ name }} v{{ version }}'
-                    ].join('\n');
-    
-    get(function(error, json) {
-        var data, arg, cmd, version;
+                    ].join('\n'),
+                    
+        Info        = tryRequire('./package'),
+        
+        arg, cmd, version;
         
         arg = args[0];
         
-        if (error) {
-            console.error(error.message);
-        } else if (!args.length) {
+        if (!args.length) {
             console.log(Util.render(MSG, {
-                name    : json.name,
-                version : json.version
+                name    : Info.name,
+                version : Info.version
             }));
-        } else if (arg === '-v' || arg === '---v') {
+        } else if (arg === '-v' || arg === '--v') {
             version = require('../package').version;
             console.log('v' + version);
-        } else if (isNaN(arg[1] - 0)) {
-            console.error('Version should be number.');
-        } else {
-            
-            if (isNaN(arg[0]) - 0)
+        } else if (!Info)
+            console.error('package.json not found.');
+        else {
+            if (arg[0] === 'v')
                 version = arg.slice(1);
             else
                 version = arg;
             
-            json.version = version;
-            
-            cmd     = Util.render(Cmd, {
+            cmd = Util.render(Cmd, {
                 version: version
             });
             
-            data    = JSON.stringify(json, 0, 2) + '\n';
-            
-            set(data, function(error) {
-                if (error)
-                    console.error(error.message);
+            spawnify(cmd, function(error, json) {
+                var err = error && error.message || json.stderr;
+                
+                if (err)
+                    console.error(err);
                 else
-                    spawnify(cmd, function(error, json) {
-                        var err = error && error.message;
-                        
-                        if (err ||  json.stderr)
-                            console.error(err || json.stderr);
-                        else
-                            console.log(json.stdout);
-                    });
+                    console.log(json.stdout);
             });
         }
-    });
-    
-    function get(callback) {
-        fs.readFile('package.json', {encoding:'utf8'}, function(error, data) {
-            var json;
-            
-            if (!error)
-                json = JSON.parse(data);
-            
-            callback(error, json);
-        });
-    }
-    
-    function set(data, callback) {
-        fs.writeFile('package.json', data, callback);
-    }
     
 })();
