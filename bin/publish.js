b16078db3a88d5414200f87f3ef77dfa81130890
coderaiser <mnemonic.enemy@gmail.com>
2014-11-03 12:38:24 -0500

    feature(package) spawnify v1.3.0

--- bin/publish.js
+++ bin/publish.js
@@ -3,20 +3,27 @@
 (function() {
     'use strict';
     
-    var publish     = require('../lib/publish'),
-        args        = process.argv.slice(2),
-        arg         = args[0],
-        version;
+    var publish         = require('../lib/publish'),
+        args            = process.argv.slice(2),
+        arg             = args[0],
+        pub, version;
         
         if (arg === '-v' || arg === '--v') {
             version = require('../package').version;
             console.log('v' + version);
        } else {
-            publish(arg, function(error, msg) {
-                if (error)
-                    console.error(error.message);
-                else
-                    console.log(msg);
+            pub = publish(arg);
+            
+            pub.on('error', function(error) {
+                console.error(error.message);
+            });
+            
+            pub.on('data', function(data) {
+                console.log(data);
+            });
+            
+            pub.on('end', function() {
+                pub = null;
             });
        }
 })();
