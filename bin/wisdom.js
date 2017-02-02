b46b9bc3851f6c6e398ab60de9d4eaf850f7d89a
coderaiser <coderaiser@cloudcmd.io>
2017-02-02 14:07:33 +0200

    chore(wisdom) rm iife

--- bin/wisdom.js
+++ bin/wisdom.js
@@ -1,55 +1,53 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var args            = process.argv.slice(2),
+    arg             = args[0];
     
-    var args            = process.argv.slice(2),
-        arg             = args[0];
+if (/^(-v|--v)$/.test(arg))
+    version();
+else if (!arg || /^(-h|--help)$/.test(arg))
+    help();
+else if (!/^(patch|minor|major)$/.test(arg))
+    console.error('\'%s\' is not a wisdom option. See \'wisdom --help\'', arg);
+else
+    main();
+   
+function main() {
+    var publish = require('..'),
+        pub     = publish(arg);
         
-    if (/^(-v|--v)$/.test(arg))
-        version();
-    else if (!arg || /^(-h|--help)$/.test(arg))
-        help();
-    else if (!/^(patch|minor|major)$/.test(arg))
-        console.error('\'%s\' is not a wisdom option. See \'wisdom --help\'', arg);
-    else
-        main();
-       
-    function main() {
-        var publish = require('..'),
-            pub     = publish(arg);
-            
-        pub.on('error', function(error) {
-            process.stderr.write(error.message);
-        });
-        
-        pub.on('data', function(data) {
-            process.stdout.write(data);
-        });
-        
-        pub.on('exit', function() {
-            pub = null;
-        });
-    }
-       
-    function version() {
-        console.log('v' + info().version);
-    }
+    pub.on('error', function(error) {
+        process.stderr.write(error.message);
+    });
     
-    function info() {
-        return require('../package');
-    }
+    pub.on('data', function(data) {
+        process.stdout.write(data);
+    });
     
-    function help() {
-        var bin         = require('../json/bin'),
-            usage       = 'Usage: ' + info().name + ' [patch|minor|major]';
-            
-        console.log(usage);
-        console.log('Options:');
+    pub.on('exit', function() {
+        pub = null;
+    });
+}
+   
+function version() {
+    console.log('v' + info().version);
+}
+
+function info() {
+    return require('../package');
+}
+
+function help() {
+    var bin         = require('../json/bin'),
+        usage       = 'Usage: ' + info().name + ' [patch|minor|major]';
         
-        Object.keys(bin).forEach(function(name) {
-            var line = '  ' + name + ' ' + bin[name];
-            console.log(line);
-        });
-    }
-})();
+    console.log(usage);
+    console.log('Options:');
+    
+    Object.keys(bin).forEach(function(name) {
+        var line = '  ' + name + ' ' + bin[name];
+        console.log(line);
+    });
+}
