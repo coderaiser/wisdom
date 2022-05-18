afbb36e37901abe77d0ae27eec2235d497075335
coderaiser <coderaiser@cloudcmd.io>
2022-05-18 21:23:51 +0300

    feature: wisdom: improve messages

--- .madrun.js
+++ .madrun.js
@@ -1,9 +1,17 @@
-import {run} from 'madrun';
+import {
+    run,
+    cutEnv,
+} from 'madrun';
+
+const NODE_OPTIONS = `'--no-warnings --loader mock-import'`;
+const testEnv = {
+    NODE_OPTIONS,
+};
 
 export default {
-    'test': () => `tape 'test/**/*.js' 'lib/**/*.spec.js'`,
-    'coverage': async () => `escover ${await run('test')}`,
-    'coverage:old': async () => `c8 --exclude="lib/**/{fixture,*.spec.js}" ${await run('test')}`,
+    'test': () => [testEnv, `tape 'test/**/*.js' 'lib/**/*.spec.js'`],
+    'coverage': async () => `escover ${await cutEnv('test')}`,
+    'coverage:old': async () => [testEnv, `c8 --exclude="lib/**/{fixture,*.spec.js}" ${await cutEnv('test')}`],
     'report': () => 'c8 report --reporter=lcov',
     'wisdom': () => 'echo wisdom: $wisdom_version',
     'patch': () => 'node bin/wisdom.js patch',
