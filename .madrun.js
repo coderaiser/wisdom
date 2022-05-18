af8eb88f7e74757bac9d72e86795296007429533
coderaiser <coderaiser@cloudcmd.io>
2022-05-18 17:13:22 +0300

    feature: wisdom: add ability to check for 'repository' field of 'package.json'

--- .madrun.js
+++ .madrun.js
@@ -2,7 +2,8 @@ import {run} from 'madrun';
 
 export default {
     'test': () => `tape 'test/**/*.js' 'lib/**/*.spec.js'`,
-    'coverage': async () => `c8 --exclude="lib/**/{fixture,*.spec.js}" ${await run('test')}`,
+    'coverage': async () => `escover ${await run('test')}`,
+    'coverage:old': async () => `c8 --exclude="lib/**/{fixture,*.spec.js}" ${await run('test')}`,
     'report': () => 'c8 report --reporter=lcov',
     'wisdom': () => 'echo wisdom: $wisdom_version',
     'patch': () => 'node bin/wisdom.js patch',
